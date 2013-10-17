b2636001740fa9f07ce4fa7cc7172b81c23b3ef9
Cameron Smith <cabotmoose@gmail.com>
2013-10-17 00:12:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express'),
     neighborhood = require('./routes/neighborhoods'),
     path = require('path'),
-    http = require('http'),
-    io = require('socket.io');
+    http = require('http');
 
 var app = express();
 
@@ -13,33 +12,12 @@ app.configure(function() {
 	app.use(express.static(path.join(__dirname, 'public')));
     });
 
-var server = http.createServer(app);
-io = io.listen(server);
-
-//io.configure(function () {
-//io.set('authorization', function (handshakedata, callback) {
-//	if (handsakedata.xdomain) {
-//	    callback('Cross-domain connections are not allowed');
-//	} else {
-//	    callback(null, true);
-//	}
-//    });
-//  });
-
-server.listen(app.get('port'), function () {
-	console.log("Express server listening on port " + app.get('port'));
-    });
-
 app.get('/neighborhoods', neighborhood.findAll);
 app.get('/neighborhoods/:id', neighborhood.findById);
 app.post('/neighborhoods', neighborhood.addNeighborhood);
 app.put('/neighborhoods/:id', neighborhood.updateNeighborhood);
 app.delete('/neighborhoods/:id', neighborhood.deleteNeighborhood);
 
-io.sockets.on('connection', function (socket) {
-	
-	socket.on('message', function (message) {
-		console.log("Got message: " + message);
-		io.sockets.emit('pageview', { 'url': message });
-	    });
+http.createServer(app).listen(app.get('port'), function() {
+	console.log("Express server listening on port " + app.get('port'));
     });
