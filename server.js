b6fbd1fabdf68515bca3377d5be8bada5fbdf4b1
Cameron Smith <cabotmoose@gmail.com>
2013-10-16 22:54:42 -0400

    Updating sockets info

--- server.js
+++ server.js
@@ -13,12 +13,33 @@ app.configure(function() {
 	app.use(express.static(path.join(__dirname, 'public')));
     });
 
+var server = http.createServer(app);
+io = io.listen(server);
+
+//io.configure(function () {
+//io.set('authorization', function (handshakedata, callback) {
+//	if (handsakedata.xdomain) {
+//	    callback('Cross-domain connections are not allowed');
+//	} else {
+//	    callback(null, true);
+//	}
+//    });
+//  });
+
+server.listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
+    });
+
 app.get('/neighborhoods', neighborhood.findAll);
 app.get('/neighborhoods/:id', neighborhood.findById);
 app.post('/neighborhoods', neighborhood.addNeighborhood);
 app.put('/neighborhoods/:id', neighborhood.updateNeighborhood);
 app.delete('/neighborhoods/:id', neighborhood.deleteNeighborhood);
 
-http.createServer(app).listen(app.get('port'), function() {
-	console.log("Express server listening on port " + app.get('port'));
+io.sockets.on('connection', function (socket) {
+	
+	socket.on('message', function (message) {
+		console.log("Got message: " + message);
+		io.sockets.emit('pageview', { 'url': message });
+	    });
     });
